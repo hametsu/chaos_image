8d27f4124a0ae9ce390e65ce22e984665a9a0384
hagino3000 <hagino.3000@gmail.com>
2010-12-04 05:34:13 +0900

    Change module name face to Face

--- common_lib/sample2.js
+++ common_lib/sample2.js
@@ -1,11 +1,11 @@
 function webGLStart() {
-  hametsu.face.on(handleNewFace);
-  hametsu.face.startTimer();
+  hametsu.Face.on(handleNewFace);
+  hametsu.Face.startTimer();
 }
 
 function handleNewFace(data) {
   data.faces.forEach(function(f) {
-    var imageUrl = hametsu.face.API_BASE + f;
+    var imageUrl = hametsu.Face.API_BASE + f;
     var image = document.createElement('image');
     image.src = imageUrl;
     document.body.appendChild(image);
