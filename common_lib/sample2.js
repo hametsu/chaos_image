d425678f9416599fe72edf114d60e21f6424a248
hagino3000 <hagino.3000@gmail.com>
2010-12-10 00:38:04 +0900

    rename directory lib -> 3rdparty

--- common_lib/sample2.js
+++ common_lib/sample2.js
@@ -1,7 +1,7 @@
 function webGLStart() {
 
   //API base is setted http://localhost:4567 by default
-  //hametsu.Face.setAPIBase('http://192.168.10.174:4567/');
+  //hametsu.Face.setAPIBase('http://192.168.1.16:4567/');
 
   hametsu.Face.on(handleNewFace);
   hametsu.Face.startTimer();
@@ -11,8 +11,13 @@ function handleNewFace(data) {
   data.faces.forEach(function(f) {
     var imageUrl = hametsu.Face.getAPIBase() + f;
     var image = document.createElement('image');
+    var firstImg;
     image.src = imageUrl;
-    document.body.appendChild(image);
+    if ((firstImg = document.querySelector('body img'))) {
+      document.body.insertBefore(image, firstImg);
+    } else {
+      document.body.appendChild(image);
+    }
   });
 }
 
