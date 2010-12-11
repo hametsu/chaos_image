14d22316786c64d4c40a145b93494c31eef736b2
hagino3000 <hagino.3000@gmail.com>
2010-12-11 10:24:52 +0900

    Fixed bugs

--- viewer/2dmain.js
+++ viewer/2dmain.js
@@ -1,76 +1,94 @@
-(function() {
-var NUM = 200;
-var WIDTH;
-var HEIGHT;
-var speedX = new Array(NUM);
-var speedY = new Array(NUM);
-var locX = new Array(NUM);
-var locY = new Array(NUM);
-var size = new Array(NUM);
-var ctx;
-var imgs = [];
+(function(namespace) {
 
-function canvasStart(faces) {
-  var canvas = document.getElementById('2d');
-  WIDTH = $(canvas).width();
-  HEIGHT = $(canvas).height();
-
-
-  faces.forEach(function(f) {
-    var img = new Image();
-    img.src = hametsu.Face.getAPIBase() + f;
-    imgs.push(img);
-  });
-
-  if ((ctx = canvas.getContext && canvas.getContext('2d'))) {
-
-    for (var i=0; i<NUM; i++) {
-      speedX[i] = Math.random() * 8.0 - 4.0;
-      speedY[i] = Math.random() * 8.0 - 4.0;
-      locX[i] = WIDTH / 2;
-      locY[i] = HEIGHT / 2;
-      size[i] = Math.random() * 40.0 + 20.0;
-    }
-    setInterval(draw, 33);
-
+  // INTERFACE
+  namespace.F2DController = {
+    start : canvasStart
   }
-}
 
-function handleNewFace(data) {
-  data.faces.forEach(function(f) {
-    imgs.pop();
-    var img = new Image();
-    img.src = hametsu.Face.getAPIBase() + f;
-    imgs.push(img);
-  });
-}
+  const MAX_FACE_NUM = 300;
+  const INTERVAL = 33;
+  var ctx;
+  var WIDTH;
+  var HEIGHT;
+  var speedX = [];
+  var speedY = [];
+  var locX = [];
+  var locY = [];
+  var size = [];
+  var imgs = [];
 
-function draw() {
+  function canvasStart(canvasName, faces) {
+    var canvas = document.getElementById(canvasName);
+    WIDTH = $(canvas).width();
+    HEIGHT = $(canvas).height();
 
+    if ((ctx = canvas.getContext && canvas.getContext('2d'))) {
 
-  ctx.globalCompositeOperation = "source-over";
-  //ctx.fillStyle = "rgba(8,8,12,0.1)";
-  //ctx.fillRect(0, 0, WIDTH, HEIGHT);
-  ctx.clearRect(0, 0, WIDTH, HEIGHT);
-  ctx.globalCompositeOperation = "lighter";
+      faces.forEach(function(f, i) {
+        var img = new Image();
+        img.src = hametsu.Face.getAPIBase() + f;
+        imgs[i] = img;
+        speedX[i] = Math.random() * 8.0 - 4.0;
+        speedY[i] = Math.random() * 8.0 - 4.0;
+        locX[i] = WIDTH / 2;
+        locY[i] = HEIGHT / 2;
+        size[i] = Math.random() * 40.0 + 20.0;
+      });
 
-  for (var i=0; i<NUM; i++) {
-    locX[i] += speedX[i];
-    locY[i] += speedY[i];
+      console.info('Initial Face count:', faces.length);
 
-
-    if(locX[i] < 0 || locX[i] > WIDTH){
-      speedX[i] *= -1.0;
+      hametsu.Face.on(handleNewFace);
+      setInterval(draw, INTERVAL);
+      return;
     }
-
-    if(locY[i] < 0 || locY[i] > HEIGHT){
-      speedY[i] *= -1.0;
-    }
-
-
-    ctx.drawImage(imgs[i], locX[i], locY[i], size[i], size[i])
-    hametsu.Face.on(handleNewFace);
+    alert('Error!! cannot get 2d context');
   }
-}
-window.start2D = canvasStart;
-})();
+
+  function handleNewFace(data) {
+    console.info('new iamges comes-------------------------');
+    data.faces.forEach(function(f) {
+      console.info('add image:', f);
+      var img = new Image();
+      img.src = hametsu.Face.getAPIBase() + f;
+      imgs.push(img);
+      speedX.push(Math.random() * 8.0 - 4.0);
+      speedY.push(Math.random() * 8.0 - 4.0);
+      locX.push(WIDTH / 2);
+      locY.push(HEIGHT / 2);
+      size.push(Math.random() * 40.0 + 20.0);
+
+      if (imgs.length > MAX_FACE_NUM) {
+        console.info('reduce images!!!!!!!!');
+        imgs.shift();
+        speedX.shift();
+        speedY.shift();
+        locX.shift();
+        locY.shift();
+        size.shift();
+      }
+    });
+  }
+
+  function draw() {
+
+    ctx.globalCompositeOperation = "source-over";
+    //ctx.fillStyle = "rgba(8,8,12,0.1)";
+    //ctx.fillRect(0, 0, WIDTH, HEIGHT);
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    ctx.globalCompositeOperation = "lighter";
+
+    for (var i=0, len=imgs.length; i<len; i++) {
+      locX[i] += speedX[i];
+      locY[i] += speedY[i];
+
+      if(locX[i] < 0 || locX[i] > WIDTH){
+        speedX[i] *= -1.0;
+      }
+
+      if(locY[i] < 0 || locY[i] > HEIGHT){
+        speedY[i] *= -1.0;
+      }
+      ctx.drawImage(imgs[i], locX[i], locY[i], size[i], size[i])
+    }
+  }
+})(window.hametsu);
--- viewer/bootstrap.js
+++ viewer/bootstrap.js
@@ -1,4 +1,31 @@
+/**
+ * Bootstrap for Chaos visiter's face viewer
+ *
+ */
 $(function() {
+
+  var wHeight = $(window).height();
+  var wWidth = $(window).width();
+
+  // For canvas2D
+  var canvas2d = document.createElement('canvas');
+  $(canvas2d).attr({
+    width : wWidth,
+    height : wHeight,
+    id : '2d'
+  }).css({
+    zIndex : 1
+  }).appendTo(document.body);
+
+  // For webGL
+  var canvas3d = document.createElement('canvas');
+  $(canvas3d).attr({
+    width : wWidth,
+    height : wHeight,
+    id : '3d'
+  }).css({
+    zIndex : 0
+  }).appendTo(document.body);
 
   function setupFaceLoader() {
 
@@ -21,12 +48,14 @@ $(function() {
     */
   }
 
-  hametsu.Face.setAPIBase("http://192.168.1.16:4567/");
+  //hametsu.Face.setAPIBase("http://192.168.1.16:4567/");
+  hametsu.Face.setAPIBase("http://localhost:4567/");
   hametsu.Face.getFaces(function(rs) {
     console.dir(rs);
     rs.faces.reverse();
-    start2D(rs.faces);
-    start3D(rs.faces);
+    hametsu.F2DController.start('2d', rs.faces);
+    hametsu.F3DController.start('3d', rs.faces);
+    hametsu.Face.startTimer();
   });
   
 });
