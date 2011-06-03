1650dade9174aef6765028bc1909717d028810c1
takano32 <mitsuhiro.takano@gree.co.jp>
2011-06-03 18:11:39 +0900

    dont generate png twice at once

--- relation/model/graph.rb
+++ relation/model/graph.rb
@@ -4,18 +4,16 @@ Sequel::Model.plugin(:schema)
 
 Sequel.connect("sqlite://model/graphs.db")
 
-require 'model/relation'
 
 class Graphs < Sequel::Model
 	def self.gyazo(data)
-		png = Relations.to_png
 		cmd = %Q[curl -s -F imagedata=@- -F id=kogaidan -H "Expect:" http://gyazo.com/upload.cgi | strings]
-		data = IO.popen(cmd, "r+") do |io|
-			io.puts(png)
+		url = IO.popen(cmd, "r+") do |io|
+			io.puts(data)
 			io.close_write
 			io.read
 		end
-		url = data
+		url
 	end
 end
 
